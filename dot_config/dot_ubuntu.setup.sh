5a5ba7a37b5ed846f55343d0d556f7ca0a96c51b
Sondre <sondreum@gmail.com>
2024-06-30 13:44:55 +0200

    fix(basrc): broken if checks

--- dot_config/dot_ubuntu.setup.sh
+++ dot_config/dot_ubuntu.setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~/
+mkdir -p ~/Documents/repo
 
 # install chezmoi
 GITHUB_USERNAME="$1"
@@ -11,11 +12,27 @@ echo "[user]
 	name = 
 	email = 
 	username = $GITHUB_USERNAME
-" > ~/.gitconfig.local
-
+" >~/.gitconfig.local
 
 # install rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# prompt Y/n for installation
+if [ -z "$1" ]; then
+	read -p "Do you want to install rustup? (Y/n) " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	fi
+fi
+
+# install tailscale
+# prompt Y/n for installation
+if [ -z "$1" ]; then
+	read -p "Do you want to install tailscale? (Y/n) " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		curl -fsSL https://tailscale.com/install.sh | sh
+	fi
+fi
 
 # install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -28,5 +45,6 @@ curl -Lo Downloads/vscode.deb "https://update.code.visualstudio.com/latest/linux
 sudo apt install .Downloads/vscode.deb
 
 # install dependencies from file
-sudo apt install -y $(cat .dep_ubuntu.txt)
+sudo apt install -y $(cat .ubuntu.apt.txt)
 
+sudo apt update && sudo apt upgrade -y
